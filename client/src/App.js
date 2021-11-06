f195a292a936ff7ac7a318426823ffdd7021780e
youssefAmmar1 <76122784+youssefAmmar1@users.noreply.github.com>
2021-11-06 21:16:24 +0200

    rename search form

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import FlightForm from "./components/FlightForm/FlightForm";
+import SearchForm from "./components/SearchForm/SearchForm";
 import FlightList from "./components/FlightList/FlightList";
 import CreateFlightForm from "./components/CreateFlightForm/CreateFlightForm";
 import ListAllFlights from "./components/ListAllFlights/ListAllFlights";
@@ -9,11 +9,10 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={FlightForm} />
+        <Route path="/" exact component={SearchForm} />
         <Route path="/list-flights" exact component={FlightList} />
         <Route path="/create-flight" exact component={CreateFlightForm} />
         <Route path="/list-all-flights" exact component={ListAllFlights} />
-
       </Switch>
     </Router>
   );
