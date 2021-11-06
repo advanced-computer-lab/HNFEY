136efb44678f5b45bcbbdce6a7d71277b1d1c06f
Farah Temraz <farahmohamedtemraz@gmail.com>
2021-11-06 13:47:21 +0200

    create flight component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import FlightForm from "./components/FlightForm/FlightForm";
 import FlightList from "./components/FlightList/FlightList";
+import CreateFlightForm from "./components/CreateFlightForm/CreateFlightForm";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={FlightForm} />
         <Route path="/list-flights" exact component={FlightList} />
+        <Route path="/create-flight" exact component={CreateFlightForm} />
       </Switch>
     </Router>
   );
