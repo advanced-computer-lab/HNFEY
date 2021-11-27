c0512f1b1091b9dd8aa2b04e1b88dbc91b6c8678
youssefAmmar1 <76122784+youssefAmmar1@users.noreply.github.com>
2021-11-27 18:42:30 +0200

    modify search

--- client/src/components/SearchResultsGuest/SearchResultsGuest.js
+++ client/src/components/SearchResultsGuest/SearchResultsGuest.js
@@ -14,11 +14,10 @@ import {
   Table,
 } from "@material-ui/core";
 
-import Card from '@mui/material/Card';
-import Container from '@mui/material/Container';
+import Card from "@mui/material/Card";
+import Container from "@mui/material/Container";
 import { useHistory } from "react-router";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 export const FlightList = () => {
   const history = useHistory();
@@ -35,8 +34,7 @@ export const FlightList = () => {
     departureDay: departureQuery.returnDate,
     passengers: departureQuery.passengers,
     class: departureQuery.class,
-  }
-
+  };
 
   const noOfKeys = Object.keys(departureQuery).length;
 
@@ -45,7 +43,6 @@ export const FlightList = () => {
     let last = i + 1 === noOfKeys ? "" : "&";
     return (departureUrl += key + "=" + value + last);
   });
-
 
   Object.entries(returnQuery).map((entry, i) => {
     let [key, value] = entry;
@@ -60,106 +57,37 @@ export const FlightList = () => {
 
   return departureFlightList && returnFlightList ? (
     <div>
-      {/* <TableContainer component={Paper} style={{ marginTop: "65px" }}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Flight Number</TableCell>
-              <TableCell align="center">From</TableCell>
-              <TableCell align="center">To</TableCell>
-              <TableCell align="center">Departure Time</TableCell>
-              <TableCell align="center">Arrival Time</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {departureFlightList?.map((flight) => (
-              <TableRow
-                key={flight._id}
-                //   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {flight.flightNumber}
-                </TableCell>
-                <TableCell align="center">{flight.from}</TableCell>
-                <TableCell align="center">{flight.to}</TableCell>
-                <TableCell align="center">
-                  {moment(flight.departureDateTime).format(
-                    "DD-MM-YYYY hh:mm A"
-                  )}
-                </TableCell>
-                <TableCell align="center">
-                  {moment(flight.arrivalDateTime).format("DD-MM-YYYY hh:mm A")}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-
-          <TableBody>
-            {returnFlightList?.map((flight) => (
-              <TableRow
-                key={flight._id}
-                //   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {flight.flightNumber}
-                </TableCell>
-                <TableCell align="center">{flight.from}</TableCell>
-                <TableCell align="center">{flight.to}</TableCell>
-                <TableCell align="center">
-                  {moment(flight.departureDateTime).format(
-                    "DD-MM-YYYY hh:mm A"
-                  )}
-                </TableCell>
-                <TableCell align="center">
-                  {moment(flight.arrivalDateTime).format("DD-MM-YYYY hh:mm A")}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-
-
-        </Table>
-      </TableContainer> */}
-
       <Container style={{ marginTop: "100px" }}>
         Choose your departure flight
-        <br/>
-        <br/>
-
-      {departureFlightList?.map((flight) => (
-        <Link to={'/'}>
+        <br />
+        <br />
+        {departureFlightList?.map((flight) => (
+          <Link to={"/"}>
             <Card style={{ width: "50%", height: "100px" }}>
-              {moment(flight.departureDateTime).format(
-                    "hh:mm A"
-                  )} - {moment(flight.arrivalDateTime).format(
-                    "hh:mm A"
-                  )}
-              <br/>
+              {moment(flight.departureDateTime).format("hh:mm A")} -{" "}
+              {moment(flight.arrivalDateTime).format("hh:mm A")}
+              <br />
               {flight.from} - {flight.to}
             </Card>
-            </Link>
-          ))}
-      </Container>
-  
-      <Container style={{ marginTop: "100px" }}>
-      Choose your return flight
-      <br/>
-      <br/>
-      {returnFlightList?.map((flight) => (
-          <Link to={'/'}>
-            <Card style={{ width: "50%", height: "100px"}} >
-              {moment(flight.departureDateTime).format(
-                    "hh:mm A"
-                  )} - {moment(flight.arrivalDateTime).format(
-                    "hh:mm A"
-                  )}
-              <br/>
-              {flight.from} - {flight.to}
-            </Card>
-            </Link>
-          ))}
+          </Link>
+        ))}
       </Container>
 
+      <Container style={{ marginTop: "100px" }}>
+        Choose your return flight
+        <br />
+        <br />
+        {returnFlightList?.map((flight) => (
+          <Link to={"/"}>
+            <Card style={{ width: "50%", height: "100px" }}>
+              {moment(flight.departureDateTime).format("hh:mm A")} -{" "}
+              {moment(flight.arrivalDateTime).format("hh:mm A")}
+              <br />
+              {flight.from} - {flight.to}
+            </Card>
+          </Link>
+        ))}
+      </Container>
     </div>
   ) : null;
 };
