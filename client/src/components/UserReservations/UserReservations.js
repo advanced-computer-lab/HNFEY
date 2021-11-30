b67218339da00704a93c2ff36fe600a219f6d829
Nour Sherif <noorsherifm@gmail.com>
2021-12-01 01:55:07 +0200

    user reservation info

--- client/src/components/UserReservations/UserReservations.js
+++ client/src/components/UserReservations/UserReservations.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { useHistory, useLocation } from "react-router";
 import queryString from "query-string";
+import { Container, Button } from "@material-ui/core";
 import Axios from "axios";
 import {
-  Button,
   TableBody,
   TableCell,
   TableRow,
@@ -14,6 +14,8 @@ import {
 } from "@material-ui/core";
 import { useEffect } from "react";
 import { CircularProgress } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import moment from "moment";
 
 
 const UserReservations = () => {
@@ -83,16 +85,106 @@ useEffect(() => {
     },[]);
 
     return user._id ? (
+          
         <div>
-          {/* {console.log(arrivalFlight.id61a13cdde995fc9671399ebe)} */}
-          {userReservations.map((reservation) => {
-            {const concat = 'id'+reservation.departingFlightId;
-             console.log(concat);
-             if(departingFlight._id)
-            console.log(departingFlight.id61890bcf7c8b767173907e47.to)}
+            <h1>My reservations</h1>
+           <TableContainer component={Paper} style={{ marginTop: "65px" }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell> Flight Number</TableCell>
+              <TableCell align="center">From</TableCell>
+              <TableCell align="center">To</TableCell>
+              <TableCell align="center">Departure Time</TableCell>
+              <TableCell align="center">Arrival Time</TableCell>
+              <TableCell align="center">Departure Terminal</TableCell>
+              <TableCell align="center">Arrival Terminal</TableCell>
+              <TableCell align="center">Cancel</TableCell>
+              
+            
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            { userReservations.map((reservation) => {
+                {const concat = 'id'+reservation.departingFlightId;
+                const concat2='id'+reservation.arrvivalFlightId;
+                console.log(arrivalFlight[concat2]);
+                if(departingFlight[concat] && arrivalFlight[concat2]){
+         return(
+          <>
+              <TableRow key={departingFlight[concat]._id}>
+                <TableCell
+                  // onClick={() => handleCellClick(flight._id)}
+                  component="th"
+                  scope="row"
+                >
+                  {departingFlight[concat].flightNumber}
+                </TableCell>
+                <TableCell align="center">{departingFlight[concat].from}</TableCell>
+                <TableCell align="center">{departingFlight[concat].to}</TableCell>
+                <TableCell align="center">
+                  {moment(departingFlight[concat].departureDateTime).format(
+                    "DD-MM-YYYY hh:mm A"
+                  )}
+                </TableCell>
+                <TableCell align="center">
+                  {moment(departingFlight[concat].arrivalDateTime).format("DD-MM-YYYY hh:mm A")}
+                </TableCell>
+                <TableCell align="center">{departingFlight[concat].departureTerminal}</TableCell>
+                <TableCell align="center">{departingFlight[concat].arrivalTerminal}</TableCell>
+                <TableCell align="center">
+                  <Button
+                    style={{ width: 100 }}
+                    variant="contained"
+                    color="primary"
+                    // onClick={(e) => handleEdit(e, flight._id)}
+                  >
+                    Cancel
+                  </Button>
+                  </TableCell>
+      </TableRow>
+     
+                    <TableRow key={arrivalFlight[concat2]._id}>
+                    <TableCell
+                  // onClick={() => handleCellClick(flight._id)}
+                  component="th"
+                  scope="row"
+                >
+                  {arrivalFlight[concat2].flightNumber}
+                  </TableCell>
+                      {console.log(arrivalFlight[concat2].from)}
+                    <TableCell align="center">{arrivalFlight[concat2].from}</TableCell>
+                      <TableCell align="center">{arrivalFlight[concat2].to}</TableCell>
+                        <TableCell align="center">
+                            {moment(arrivalFlight[concat2].departureDateTime).format(
+                              "DD-MM-YYYY hh:mm A"
+                            )}
+                          </TableCell>
+                          <TableCell align="center">
+                            {moment(arrivalFlight[concat2].arrivalDateTime).format("DD-MM-YYYY hh:mm A")}
+                          </TableCell>
+                          <TableCell align="center">{arrivalFlight[concat2].departureTerminal}</TableCell>
+                          <TableCell align="center">{arrivalFlight[concat2].arrivalTerminal}</TableCell>
+                          <TableCell align="center">
+                            <Button
+                              style={{ width: 100 }}
+                              variant="contained"
+                              color="primary"
+                              // onClick={(e) => handleEdit(e, flight._id)}
+                            >
+                              Cancel
+                            </Button>
+                            </TableCell>
+                    </TableRow>
+      </>
+                    
+        )}}})}
 
-          })}
-        </div>
+            
+          </TableBody>
+          </Table>
+          </TableContainer>
+          </div>
       )
       : (
         <CircularProgress />
