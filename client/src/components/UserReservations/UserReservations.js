31afc5f8d385680872f7dbc562095be37b32f386
Nour Sherif <noorsherifm@gmail.com>
2021-12-02 15:45:58 +0200

    delete reservation confirmation

--- client/src/components/UserReservations/UserReservations.js
+++ client/src/components/UserReservations/UserReservations.js
@@ -16,6 +16,7 @@ import { useEffect } from "react";
 import { CircularProgress } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import moment from "moment";
+import { confirmAlert } from "react-confirm-alert";
 
 const UserReservations = (props) => {
   const history = useHistory();
@@ -106,7 +107,24 @@ const UserReservations = (props) => {
     }
   }, [userReservations]);
 
+  const cancel = (e,reservationId) => {
+    confirmAlert({
+      title: "Are you sure you want to cancel this reservation?",
+      message: "Are you sure you want to cancel this reservation?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => handleCancel(e,reservationId),
+        },
+        {
+          label: "No",
+          onClick: () => {},
+        },
+      ],
+    });
+  };
   const handleCancel = async (e, reservationId) => {
+    
     e.preventDefault();
 
     const reservation = {
@@ -230,7 +248,7 @@ const UserReservations = (props) => {
           }}
           variant="contained"
           color="primary"
-          onClick={(e) => handleCancel(e, userReservations._id)}
+          onClick={(e) => cancel(e, userReservations._id)}
           disabled={cancelPressed ? true : false}
         >
           {cancelPressed ? "Cancelled" : "Cancel"}
