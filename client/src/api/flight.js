952f0b12963011529d178f223fe0cdfa3890b6c3
Nour Sherif <noorsherifm@gmail.com>
2021-12-24 20:59:38 +0200

    routes fixing

--- client/src/api/flight.js
+++ client/src/api/flight.js
@@ -2,3 +2,9 @@ import API from "./index";
 
 export const editFlight = (flightBody) =>
   API.put("/flight/edit-flight", flightBody);
+
+export const findFlight = (url)=>
+  API.get(url);
+
+  export const cancelFlight = (details) =>
+API.post("flight/cancel-flight",details);
--- client/src/api/reservation.js
+++ client/src/api/reservation.js
@@ -2,3 +2,13 @@ import API from "./index";
 
 export const findAllReservations = (userId) =>
   API.get("/reservation/find-reservation?userId=" + userId);
+
+export const findReservation = (reservationId) =>
+  API.get("/reservation/" + reservationId);
+
+export const editReservation = (reservation) =>
+API.put("/reservation/edit-reservation",reservation)
+
+
+
+
